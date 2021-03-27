bb6ed4660330b4b5c83abbe8e00d61b9a2c1a510
SaminYeaser <saminyeaser1@gmail.com>
2021-03-28 03:37:25 +0600

    authentication added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,28 @@ import { Route, Switch }  from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import HeaderComponent from './components/header-component/header.component'
 import SignInSignup from './pages/signin-signup/signin-signup.component'
+import { auth } from './components/firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-      <HeaderComponent/>
-      <Switch>
-        <Route exact={true} path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInSignup}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component() {
+
+  constructor(){
+    super();
+    this.state = {
+      currentUser : null
+    }
+  }
+  render(){
+    return (
+      <div>
+        <HeaderComponent/>
+        <Switch>
+          <Route exact={true} path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInSignup}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
