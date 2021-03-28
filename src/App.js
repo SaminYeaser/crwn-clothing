32e66c1f56323ecbf778eaaf566f569c76642d6f
SaminYeaser <saminyeaser1@gmail.com>
2021-03-29 03:02:44 +0600

    added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,24 @@ import HeaderComponent from './components/header-component/header.component'
 import SignInSignup from './pages/signin-signup/signin-signup.component'
 import { auth } from './components/firebase/firebase.utils'
 
-class App extends React.Component() {
+class App extends React.Component{
 
   constructor(){
     super();
     this.state = {
       currentUser : null
     }
+  }
+  unSubscribeFromAuth = null
+  componentDidMount(){
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user=>{
+      this.setState({currentUser: user})
+
+      console.log(user)
+    })
+  }
+  componentWillUnmount(){
+    this.unSubscribeFromAuth();
   }
   render(){
     return (
